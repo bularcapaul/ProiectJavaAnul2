a016dd89d83bf1fecbd2ded53d9d5f6dd2c68852
bularcapaul <56611331+bularcapaul@users.noreply.github.com>
2019-10-17 17:15:53 +0300

    Update Main.java

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,5 +1,32 @@
 package utils;
-
+import java.util.Scanner;
 public class Main {
+	
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nota este:");
+		Integer nota = sc.nextInt();
+		System.out.println("Alegeti materia:");
+		System.out.println("1-DE");
+		System.out.println("2-SS");
+		System.out.println("3-Engleza");
+		System.out.println("4-MS");
+		Integer optiuni = sc.nextInt();
+		
+		
+		switch(optiuni)
+		{
+		case 1 : 		System.out.println("Nota la DE este:" +nota);
+		break;
+		case 2 : 		System.out.println("Nota la SS este:" +nota);
+		break;
+		case 3 : 		System.out.println("Nota la engleza este:" +nota);
+		break;
+		case 4 : 		System.out.println("Nota la MS este:" +nota);
+		break;
+		}
+		
+	}		
 
 }
